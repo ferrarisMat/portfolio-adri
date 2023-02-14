a0ceb30e43fcaa61c25bbc5a48c70bb5e53a5e13
Mathieu Ferraris <mathieu.ferraris@autodesk.com>
2023-02-14 15:17:16 -0500

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,15 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: [],
+  // add your custom rules here
+  rules: {},
+}
---
+++ components/LocationItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="place">
+    <img :src="img" width="235" height="300" />
+    <h2>{{ name }}</h2>
+    <p>{{ desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
