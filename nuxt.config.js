f343cdecb5a4901683cf41c84e6a4080790cde13
t-ferrm <mathieu.ferraris@autodesk.com>
2023-06-28 12:31:20 -0400

    chore(config): add rules

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,12 +5,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'portfolio-adri',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
+    meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }, { hid: 'description', name: 'description', content: '' }, { name: 'format-detection', content: 'telephone=no' }],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
@@ -33,7 +28,7 @@ export default {
   ],
 
   styleResources: {
-    scss: ['~style/mixins.scss', '~style/variables.scss'],
+    scss: ['~style/reset.scss', '~style/mixins.scss', '~style/variables.scss', '~style/fonts.scss', '~style/core.scss'],
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -64,4 +59,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-}
+};
