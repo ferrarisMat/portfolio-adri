92bbd3c73e2d0eed4c0bde8974e13dad0e259b70
t-ferrm <mathieu.ferraris@autodesk.com>
2023-06-28 17:57:01 -0400

    feat(Background): add new comp

---
+++ components/Background.vue
@@ -0,0 +1,48 @@
+<template>
+	<svg
+		width="1920px"
+		height="1080px"
+		viewBox="0 0 1920 1080"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink">
+		<title>Group</title>
+		<defs>
+			<rect id="path-1" x="0" y="0" width="1920" height="1080"></rect>
+			<linearGradient id="linearGradient-3" x1="50%" y1="-1.65316667%" x2="50%" y2="100.033398%">
+				<stop stop-color="#9D0862" stop-opacity="0.445" offset="0%"></stop>
+				<stop stop-color="#A70B61" stop-opacity="0.4293" offset="3.524333%"></stop>
+				<stop stop-color="#BE115D" stop-opacity="0.3818" offset="14.19%"></stop>
+				<stop stop-color="#D0175A" stop-opacity="0.3275" offset="26.4%"></stop>
+				<stop stop-color="#DD1A58" stop-opacity="0.2632" offset="40.86%"></stop>
+				<stop stop-color="#E51C57" stop-opacity="0.179" offset="59.78%"></stop>
+				<stop stop-color="#E71D57" stop-opacity="0" offset="100%"></stop>
+			</linearGradient>
+		</defs>
+		<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+			<g id="Group">
+				<mask id="mask-2" fill="white">
+					<use xlink:href="#path-1"></use>
+				</mask>
+				<use id="Rectangle" fill="#18003F" fill-rule="nonzero" xlink:href="#path-1"></use>
+				<rect
+					id="Rectangle"
+					fill="url(#linearGradient-3)"
+					fill-rule="nonzero"
+					mask="url(#mask-2)"
+					x="0"
+					y="-574.8"
+					width="1920"
+					height="1080"></rect>
+			</g>
+		</g>
+	</svg>
+</template>
+
+<script>
+export default {
+	name: 'BackGround',
+};
+</script>
+
+<style lang="scss" scoped></style>
