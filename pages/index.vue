b274609a237d8cf98d2bf4819e7a58356de1dd15
t-ferrm <mathieu.ferraris@autodesk.com>
2023-06-28 12:33:04 -0400

    wip(index): integrate illustrations

--- pages/index.vue
+++ pages/index.vue
@@ -1,93 +1,120 @@
 <template>
   <div id="app">
-    <h1>Your Upcoming Destinations</h1>
-    <div class="location-contain">
-      <locations-wrapper>
-        <location-item
-          v-for="location in locations"
-          :key="location.img"
-          :img="location.img"
-          :name="location.name"
-          :desc="location.desc"
-        ></location-item>
-      </locations-wrapper>
-    </div>
+    <header-comp :is-logo-visible="isLogoVisible" :is-open="isOpen" @onMenuClick="onMenuClick" @onCloseMenu="onCloseMenu"></header-comp>
+    <h1 id="#" class="logo-big">
+      <nuxt-link to="/">
+        <span>Adrien Tardy</span>
+        <logo></logo>
+      </nuxt-link>
+    </h1>
+    <section id="highlighted">
+      <nexten class="illustration-item"></nexten>
+      <electronic-billing-illu class="illustration-item"></electronic-billing-illu>
+      <axon-jay-illu class="illustration-item"></axon-jay-illu>
+    </section>
   </div>
 </template>
 
 <script>
-import LocationItem from '~/components/LocationItem.vue'
-import LocationsWrapper from '~/components/LocationsWrapper.vue'
+import Logo from '../components/Logo.vue'
+import HeaderComp from '~/components/HeaderComp.vue'
+import Nexten from '~/components/NextenIllu.vue'
+import ElectronicBillingIllu from '~/components/ElectronicBillingIllu.vue';
+import AxonJayIllu from '~/components/AxonJayIllu.vue';
+
+function setLogoVisible(el, top, height){
+  if(window.scrollY > (top + height - 20)){
+    el.isLogoVisible = true;
+  } else {
+    el.isLogoVisible = false;
+  }
+}
+
 export default {
   name: 'IndexPage',
-  components: { LocationItem, LocationsWrapper },
+  components: {
+    Logo,
+    HeaderComp,
+    Nexten,
+    ElectronicBillingIllu,
+    AxonJayIllu,
+  },
   data() {
     return {
-      locations: [
-        {
-          name: 'moscow',
-          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg',
-          desc: `Moscow
-is the capital and most populous city of Russia, with 13.2 million residents
-within the city limits and 17.8 million within the urban area. Moscow has the
-status of a Russian federal city.`
-        },
-        {
-          name: 'paris',
-          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg',
-          desc: `Paris is
-the capital and most populous city of France. By the 17th century, Paris was one
-of Europe's major centres of finance, commerce, fashion, science, and the arts,
-and it retains that position still today.`
-        },
-        {
-          name: 'rome',
-          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg',
-          desc: `Rome's
-history spans more than 2,500 years. While Roman mythology dates the founding of
-Rome at around 753 BC, the site has been inhabited for much longer, making it
-one of the oldest continuously occupied sites in Europe.`
-        },
-        {
-          name: 'paris',
-          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg',
-          desc: `By
-the end of the 12th century, Paris had become the political, economic,
-religious, and cultural capital of France. Maurice de Sully undertook the
-construction of the Notre Dame Cathedral at its eastern extremity.`
-        }
-      ]
+      isLogoVisible: false,
+      isOpen: false,
     }
-  }
+  },
+  mounted(){
+    const logoBig = document.getElementsByClassName("logo-big")[0];
+    const logoBigTop = logoBig.getBoundingClientRect().top;
+    const logoBigHeight = logoBig.getBoundingClientRect().height;
+
+    setLogoVisible(this, logoBigTop, logoBigHeight)
+
+    window.addEventListener('scroll', () => {
+      setLogoVisible(this, logoBigTop, logoBigHeight)
+    })
+    window.addEventListener('resize', () => {
+      setLogoVisible(this, logoBigTop, logoBigHeight)
+    })
+  },
+  destroyed() {
+    window.removeEventListener('scroll', () => {})
+  },
+  methods: {
+    onCloseMenu(){
+      this.isOpen = false;
+      document.body.style.overflow = "visible";
+    },
+    onMenuClick(){
+      this.isOpen = !this.isOpen;
+      if(this.isOpen){
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "visible";
+      }
+    }
+  },
 }
 </script>
 
 <style lang="scss">
+html {
+  background: $main-bg-color;
+  scroll-behavior: smooth;
+}
+html {
+  overflow-x: hidden;
+}
 body {
   width: 100vw;
   height: 100vh;
   font-family: 'NTR', sans-serif;
-  background: #eee;
+  @include main-bg;
+  min-height: 5679px;
 }
-h1 {
+#app {
+  padding-top: 60px;
+  max-width: 1320px;
+  margin: 0 auto;
+  position: relative;
+}
+.logo-big {
   text-align: center;
+  width: 290px;
+  margin: 0 auto;
+  margin-top: 120px;
+  span {
+    @include visually-hidden
+  }
 }
-.place {
-  display: flex;
-  flex-direction: column;
-  width: 280px;
-  justify-content: center;
-  background: white;
-  border: 1px solid #ddd;
-  padding: 20px 20px;
-  margin: 10px;
-  h2 {
-    margin: 0;
-    text-align: center;
-  }
-  img {
-    margin: 10px;
-    align-self: center;
-  }
+
+#highlighted {
+  margin-top: 114px;
+}
+
+.illustration-item {
+  margin: 114px 0;
 }
 </style>
